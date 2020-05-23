d3da7dd294ec3abfb47f102c9ce8cd19b36acfe7
Andre Ferrer <andrerferrer@hotmail.com>
2020-05-23 16:05:22 -0300

    add validation for username

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # Add validation
+  validates :username, uniqueness: true
 end
